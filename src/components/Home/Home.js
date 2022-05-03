9abd35100c762dd8580bf37a7c1e76b33831134d
Darudul <darudulislam@gmail.com>
2022-05-04 04:33:06 +0800

    added off service

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router";
 import Banner from "../Banner/Banner";
 import Products from "../Products/Products";
+import SaveMoney from "../SaveMoney/SaveMoney";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -12,6 +13,7 @@ const Home = () => {
     <div className="position-relative">
       <Banner></Banner>
       <Products></Products>
+      <SaveMoney></SaveMoney>
       <button onClick={ManageInventory} className="position-absolute bottom-0 end-0 border-0 bg-success p-2 rounded-pill text-white px-3 fw-bold">Manage Inventories</button>
     </div>
   );
