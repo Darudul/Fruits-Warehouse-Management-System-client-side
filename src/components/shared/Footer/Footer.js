081c07535ab0786fd7437b17b80f612a85d92cfb
Darudul <darudulislam@gmail.com>
2022-05-04 08:22:57 +0800

    added footer

--- src/components/shared/Footer/Footer.js
+++ src/components/shared/Footer/Footer.js
@@ -1,9 +1,51 @@
-import React from 'react';
-
+import React from "react";
+import "./Footer.css";
 const Footer = () => {
   return (
-    <div>
-     <h1>foorerr</h1>
+    <div className=" mt-5">
+      <div className="background ">
+        <div className="footer-margin">
+          <h4 className="text-white">Store Location</h4>
+          <h6 className="text-white-50">Address:</h6>
+          <p className="text-white-50">
+            8, Jalan Bukit Pantai, 59100 Kuala Lumpur, Malaysia
+          </p>
+          <h6 className="text-white-50">Email</h6>
+          <p className="text-white-50">darudulislam@gmail.com</p>
+          <h6 className="text-white-50">Phone:</h6>
+          <p className="text-white-50">+603-22960416</p>
+        </div>
+        <div className="footer-margin">
+          <h4 className="text-white">Useful Links</h4>
+          <p className="text-white-50">About us</p>
+          <p className="text-white-50">Blogs</p>
+          <p className="text-white-50">Check out</p>
+          <p className="text-white-50">Contact</p>
+          <p className="text-white-50">Manage Inventories</p>
+        </div>
+        <div className="footer-margin">
+          <h4 className="text-white">Categories</h4>
+          <p className="text-white-50">Fruits</p>
+          <p className="text-white-50">Package Foods</p>
+          <p className="text-white-50">Health and Wellness</p>
+        </div>
+        <div className="footer-margin">
+          <h4 className="text-white">Opening Hours</h4>
+          <h6 className="text-white-50">Monday-Saturday</h6>
+          <p className="text-white-50">1200-14.45</p>
+          <h6 className="text-white-50">Sunday</h6>
+          <p className="text-white-50">15.00</p>
+        </div>
+        <div>
+          <p>Copyright 2022, Groxi Store. All Rights Reserved.</p>
+          <div>
+            <p>
+              Follow Us:
+              <p></p>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
